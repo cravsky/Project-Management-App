1a95dc0d07f535b793b49b3449d5e11f44379885
cravsky <cravsky@gmail.com>
2024-05-20 10:34:31 +0200

    Styled Input and NewProject

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,27 +1,15 @@
 import Input from "./Input"
 
 export default function NewProject() {
-    return <div>
-        <menu>
-            <li><button>Cancel</button></li>
-            <li><button>Save</button></li>
+    return (<div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+            <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
         <div>
             <Input label="Title"></Input>
             <Input label="Description" textarea></Input>
             <Input label="Due Date"></Input>
-            {/* <p>
-                <label></label>
-                <input>Title</input>
-            </p>
-            <p>
-                <label>Description</label>
-                <textarea></textarea>
-            </p>
-            <p>
-                <label>Due Date</label>
-                <input></input>
-            </p> */}
         </div>
-    </div>
+    </div>)
 }
