99e9bf093db96e53a9fed834cc378f1b50d9172b
cravsky <cravsky@gmail.com>
2024-05-22 11:14:24 +0200

    Created modal with useImperativeHandle

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,40 +1,56 @@
 import { useRef } from "react";
 
 import Input from "./Input"
+import Modal from "./Modal";
 
-export default function NewProject({onAdd}) {
+export default function NewProject({ onAdd }) {
+    const modal = useRef();
 
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
 
     function handleSave() {
-      const enteredTitle = title.current.value;
-      const enteredDescription = description.current.value;
-      const enteredDueDate = dueDate.current.value;
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
 
-      // validation ...
-      onAdd({
-        title: enteredTitle,
-        description: enteredDescription,
-        dueDate: enteredDueDate
-      })
+        if (enteredTitle.trim() === '' ||
+            enteredDescription.trim() === '' ||
+            enteredDueDate.trim() === '') {
+            modal.current.open();
+            return;
+        }
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        })
     }
 
-    return (<div className="w-[35rem] mt-16">
-        <menu className="flex items-center justify-end gap-4 my-4">
-            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-            <li>
-                <button
-                    onClick={handleSave}
-                    className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save
-                </button>
-            </li>
-        </menu>
-        <div>
-            <Input type="text" ref={title} label="Title"></Input>
-            <Input ref={description} label="Description" textarea></Input>
-            <Input type="date" ref={dueDate} label="Due Date"></Input>
-        </div>
-    </div>)
+    return (
+        <>
+            <Modal ref={modal} buttonCaption="Close">
+                <h2>Invalid input</h2>
+                <p>Please provide value for every input field</p>
+            </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                    <li>
+                        <button
+                            onClick={handleSave}
+                            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input type="text" ref={title} label="Title"></Input>
+                    <Input ref={description} label="Description" textarea></Input>
+                    <Input type="date" ref={dueDate} label="Due Date"></Input>
+                </div>
+            </div>
+        </>
+    )
 }
